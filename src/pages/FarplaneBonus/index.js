5cdade3d819a2c419f6604f77d9b2bf0c722fc42
henriquebg88 <henriquebg88@gmail.com>
2020-05-30 18:02:44 -0300

    Layout changes

--- src/pages/FarplaneBonus/index.js
+++ src/pages/FarplaneBonus/index.js
@@ -10,33 +10,35 @@ export default function FarplaneBonusPage() {
 
     return (
         <main>
-            {/* Bonus do dia atual */}
-            <div id='today_bonus'>
-                <h2>Farplane Bônus</h2>
-                {/* <h3>{call_bonus(new Date().getDay()).week_day}</h3> */}
-                {/* Bonus de hoje */}
-                <div>
-                    <p>{bonusToday.bonus_description}</p>
+            <h2>Farplane Bônus</h2>
+            <section className="farplane">
+                {/* Bonus do dia atual */}
+                <div id='today_bonus'>
+                    {/* <h3>{call_bonus(new Date().getDay()).week_day}</h3> */}
+                    {/* Bonus de hoje */}
                     <div>
-                        <div className={'elemento ' + bonusToday.element1}></div>
-                        <div className={'elemento ' + bonusToday.element2}></div>
+                        <p>{bonusToday.bonus_description}</p>
+                        <div>
+                            <div className={'elemento ' + bonusToday.element1}></div>
+                            <div className={'elemento ' + bonusToday.element2}></div>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            {/* Bonus dos proximos dias */}
-            <h3>Próximos bonûs diários:</h3>
-            <ul>
-                {next_bonus_days(serverTime_object.weekday()).map((day) => {
-                    return (
-                        <li key={day}>
-                            <h3>{call_bonus(day).week_day}</h3>
-                            <p>{call_bonus(day).bonus_description}</p>
-                        </li>
-                    )
-                })}
-            </ul>
-            <p className='small-info'>*Apesar de o jogo não mostrar, os mesmos bonûs contemplam o Multi.</p>
+                {/* Bonus dos proximos dias */}
+                <h3>Próximos bonûs diários:</h3>
+                <ul>
+                    {next_bonus_days(serverTime_object.weekday()).map((day) => {
+                        return (
+                            <li key={day}>
+                                <h3>{call_bonus(day).week_day}</h3>
+                                <p>{call_bonus(day).bonus_description}</p>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <p className='small-info'>*Apesar de o jogo não mostrar, os mesmos bonûs contemplam o Multi.</p>
+            </section>
         </main>
     )
 }
--- src/pages/guildInfo/index.js
+++ src/pages/guildInfo/index.js
@@ -1,40 +1,45 @@
 import React from 'react';
 
+import './styles.css';
+
 export default function GuildInfo() {
     return (
-        <section>
+        <main>
 
-            <h2>Nyx-star-br</h2>
+                <h2>Nyx-star-br</h2>
+            <section className="guild-info">
 
-            <div className='info-block'>
-                <h3>Estátua atual</h3>
-                <p>Estátua de Kirin</p>
-            </div>
+                <div className='info-block'>
+                    <h3>Estátua atual</h3>
+                    <p>Estátua de Kirin</p>
+                </div>
 
-            <div className="info-block">
-                <h3>Mensagens importantes</h3>
-                <ul>
-                    <li>
-                        Não deixe de doar para a estátua todos os dias.
-                    </li>
-                    <li>
-                        Ataque SEMPRE. Mesmo que não tenha chances de ganhar. Você ajuda diminuindo a vida dos oponentes ou revelando as unidades ocultas.
-                    </li>
-                    <li>
-                        Você pode fazer 2 ataques por dia. Faça os dois se seu time estiver vivo!
-                    </li>
-                    <li>
-                        Não esqueça de montar seus dois times! O de ataque e de defesa também!
-                    </li>
-                    <li>
-                        Se você tem um Rank mais alto, faça seu ataque no final do dia. Deixe os alvos mais fáceis para os mais fracos. Você pode conseguir ganhar estrelas de um alvo mais forte, mas um companheiro mais fraco não.
-                    </li>
-                    <li>
-                        Temos mais dicas, estratégias e detalhes nos outros links. Confira!
-                    </li>
-                </ul>
-            </div>
+                <div className="info-block">
+                    <h3>Mensagens importantes</h3>
+                    <ul>
+                        <li>
+                            Não deixe de doar para a estátua todos os dias.
+                        </li>
+                        <li>
+                            Ataque SEMPRE. Mesmo que não tenha chances de ganhar. Você ajuda diminuindo a vida dos oponentes ou revelando as unidades ocultas.
+                        </li>
+                        <li>
+                            Você pode fazer 2 ataques por dia. Faça os dois se seu time estiver vivo!
+                        </li>
+                        <li>
+                            Não esqueça de montar seus dois times! O de ataque e de defesa também!
+                        </li>
+                        <li>
+                            Se você tem um Rank mais alto, faça seu ataque no final do dia. Deixe os alvos mais fáceis para os mais fracos. Você pode conseguir ganhar estrelas de um alvo mais forte, mas um companheiro mais fraco não.
+                        </li>
+                        <li>
+                            Temos mais dicas, estratégias e detalhes nos outros links. Confira!
+                        </li>
+                    </ul>
+                </div>
 
-        </section>
+            </section>
+
+        </main>
     )
 }
